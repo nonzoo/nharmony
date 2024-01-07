669831e986227e5a874b5897f9439e8f14d11cc2
nonzoo <nonsoosadebe202@gmail.com>
2024-01-07 14:46:59 +0100

    test

--- naija_backend/post/api.py
+++ naija_backend/post/api.py
@@ -17,7 +17,7 @@ def post_list(request):
 
     posts = Post.objects.filter(created_by_id__in=list(user_ids))
     trend = request.GET.get('trend', '')
-
+ 
     if trend:
         posts = posts.filter(body__icontains='#' + trend)
 
